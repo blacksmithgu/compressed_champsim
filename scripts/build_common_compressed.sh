d8de660b1e5eb33d8aa3a5923643cca462c6ce70
Michael Brenan <msbrenan@cs.utexas.edu>
2019-03-23 11:25:00 -0500

    Add compressed RRIP implementation

--- scripts/build_common_compressed.sh
+++ scripts/build_common_compressed.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Builds 'common' compressed and uncompressed policies.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CHAMPSIM=${SCRIPT_DIR}/..
 
 # Uncompressed: Hawkeye, LRU
-${SCRIPT_DIR}/../build_champsim.sh --uncompressed --policy hawkeye_final --name hawkeye
-${SCRIPT_DIR}/../build_champsim.sh --uncompressed --policy lru --name lru
+${CHAMPSIM}/build_champsim.sh --uncompressed --policy hawkeye_final --name hawkeye
+${CHAMPSIM}/build_champsim.sh --uncompressed --policy lru --name lru
 
-# Compressed: clru, chawkeye (OPTgen), chawkeye (YACCgen)
-${SCRIPT_DIR}/../build_champsim.sh --policy clru --name clru
-${SCRIPT_DIR}/../build_champsim.sh --policy chawkeye --name chawkeye_optgen -- -DCACHEGEN="OPTgen\<1024\>"
-${SCRIPT_DIR}/../build_champsim.sh --policy chawkeye --name chawkeye_yaccgen -- -DCACHEGEN="YACCgen\<1024\>"
-${SCRIPT_DIR}/../build_champsim.sh --policy chawkeye --name chawkeye_coptgen -- -DCACHEGEN="UnboundedSizeAwareOPTgen"
+# Compressed: clru, crrip, chawkeye (YACCgen)
+${CHAMPSIM}/build_champsim.sh --policy clru --name clru
+${CHAMPSIM}/build_champsim.sh --policy crrip --name crrip
+${CHAMPSIM}/build_champsim.sh --policy chawkeye --name chawkeye_yaccgen -- -DCACHEGEN="YACCgen\<1024\>"
