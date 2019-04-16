d0ccfd9e2a6b2f045eb19426f0c6de3ffd23daa9
Michael Brenan <msbrenan@cs.utexas.edu>
2019-04-16 18:25:08 -0500

    Fix ./scripts/build_common_nosb.sh

--- scripts/build_common_nosb.sh
+++ scripts/build_common_nosb.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CHAMPSIM=${SCRIPT_DIR}/..
 
 # Compressed: clru, crrip, chawkeye (YACCgen), camp
-${CHAMPSIM}/build_champsim.sh --superblock no --policy clru --name clru-nosb
-${CHAMPSIM}/build_champsim.sh --superblock no --policy crrip --name crrip-nosb
-${CHAMPSIM}/build_champsim.sh --superblock no --policy chawkeye --name chawkeye-nosb
-${CHAMPSIM}/build_champsim.sh --superblock no --policy camp --name camp --name camp-nosb
+${CHAMPSIM}/build_champsim.sh --no-superblock --policy clru --name clru-nosb
+${CHAMPSIM}/build_champsim.sh --no-superblock --policy crrip --name crrip-nosb
+${CHAMPSIM}/build_champsim.sh --no-superblock --policy chawkeye --name chawkeye-nosb
+${CHAMPSIM}/build_champsim.sh --no-superblock --policy camp --name camp --name camp-nosb
