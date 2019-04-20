d3caee51848e0a43ac254db80768b75b3655545c
Jack Youstra <jyoustra@cs.utexas.edu>
2019-04-20 15:19:54 -0500

    Added some tests, not all of them are passing atm

--- scripts/interval2ilp.py
+++ scripts/interval2ilp.py
@@ -5,94 +5,125 @@ import numpy as np
 import heapq as hq
 import itertools as it
 
-parser = ArgumentParser(description="Creates an ILP from a CSV")
-parser.add_argument('csv', type=FileType('r'), help='The source intervals to create the ILP from. Must be a readable file.')
+class Interval:
+    def __init__(self, start, end, cf):
+        self.start = start
+        self.end = end
+        self.cf = cf
 
-args = parser.parse_args()
+    def __str__(self):
+        return "(start: {} end: {} x{})".format(self.start, self.end, self.cf)
 
-intervalreader = csv.reader(args.csv)
-headers = next(intervalreader, None)
-assert(headers[0] == 'start' and headers[1] == 'end' and headers[2] == 'cf' and len(headers) == 3)
+    def __repr__(self):
+        return str(self)
 
-#start = row[0]
-#end = row[1]
-#cf = row[2]
-
-def rankIntervals(x, y):
-    if x[0] == y[0]:
-        return x[1] > y[1]
-    return x[0] > y[0]
+    def to_tuple(self):
+        return (self.start, self.end, self.cf)
 
 # returns gapless access numbers
 # we get this by default, we don't need it
-def transformToNormalized(intervals):
+def transformToNormalizedIntervals(integer_interval_tuples):
+    intervals = [Interval(x, y, z) for x, y, z in integer_interval_tuples]
+
     current_quanta = 0
-    current_quanta_time = 0
-    retIntervals = []
-    # a priority queue for the intervals
-    # (end_time, start_time, cf) tuples to sort correctly on the minheap
-    open_intervals = []
-    for interval in intervals:
-        start = interval[0]
-        end = interval[1]
-        cf = interval[2]
-        while len(open_intervals) > 0 and start >= open_intervals[0][0]:
-            current_end_time, start_quanta, current_cf = hq.heappop(open_intervals)
-            if current_end_time > current_quanta_time:
-                current_quanta_time = current_end_time
-                current_quanta += 1
+    startTimes = sorted(intervals, key=lambda x: x.start)
+    endTimes = sorted(intervals, key=lambda x: x.end)
+    print("Start times: ", end='')
+    print(startTimes)
+    print("End times: ", end='')
+    print(endTimes)
 
-            retIntervals.append(start_quanta, current_quanta, current_cf)
-        if start > current_quanta_time:
-            current_quanta_time = start
+    startIndex = 0
+    endIndex = 0
+    while startIndex < len(startTimes) or endIndex < len(endTimes):
+        # starts are earlier, set these first
+        while startIndex < len(startTimes) and (endIndex == len(endTimes) or startTimes[startIndex].start <= endTimes[endIndex].end):
+            startTimes[startIndex].start = current_quanta
+            startIndex += 1
             current_quanta += 1
-        hq.heappush(open_intervals, (end, current_quanta, cf))
 
-    return retIntervals, current_quanta
+        while endIndex < len(endTimes) and (startIndex == len(startTimes) or startTimes[startIndex].start >= endTimes[endIndex].end):
+            endTimes[endIndex].end = current_quanta
+            endIndex += 1
+            current_quanta += 1
 
-intervals = sorted(intervalreader, rankIntervals)
-biggest_interval = max(intervals, lambda x, y: x[1] > y[1])
+    return intervals, current_quanta - 1
 
-# this is run per set
-# indicator variable per interval
-# interval is [start, end]
-# number variable per line type
-# a priority queue for the intervals
-# (end time, cf, indicator) tuples to sort correctly on the minheap
-open_intervals = []
+def optimalForNormalizedIntervals(intervals, set_size=16):
+    # this is run per set
+    # indicator variable per interval
+    # interval is [start, end]
+    # number variable per line type
+    # a priority queue for the intervals
+    # (end time, cf, indicator) tuples to sort correctly on the minheap
+    open_intervals = []
+    number_of_timesteps = 2 * len(intervals) - 1
 
-indicator = cp.Variable(len(intervals))
-q1 = cp.Variable(len(total_quanta))
-q2 = cp.Variable(len(total_quanta))
-x1 = cp.Variable(len(total_quanta))
-x2 = cp.Variable(len(total_quanta))
-x4 = cp.Variable(len(total_quanta))
+    indicator = cp.Variable(len(intervals), integer=True)
+    q4        = cp.Variable(number_of_timesteps, integer=True)
+    q2        = cp.Variable(number_of_timesteps, integer=True)
+    x1        = cp.Variable(number_of_timesteps, integer=True)
+    x2        = cp.Variable(number_of_timesteps, integer=True)
+    x4        = cp.Variable(number_of_timesteps, integer=True)
 
-indicators_per_quanta = np.zeros((len(),), dt=np.obj)
+    # transform into normalized access times
+    var_constraints = []
+    for rowIdx in range(len(intervals)):
+        row = intervals[rowIdx]
+        # close everything before this
+        while len(open_intervals) > 0 and row.start >= open_intervals[0][0]:
+            current_end_time, current_cf, current_indicator = hq.heappop(open_intervals)
 
-# transform into normalized access times
-for rowIdx in range(len(intervals)):
-    row = intervals[rowIdx]
-    # no validation on input
-    start = row[0]
-    end = row[1]
-    cf = row[2]
-    # close everything before this
-    while len(open_intervals) > 0 and start >= open_intervals[0][0]:
-        current_end_time, current_cf = hq.heappop(open_intervals)
+        hq.heappush(open_intervals, (row.end, row.cf, indicator[rowIdx]))
+        # output constraints at current time
+        # equations 1x, 2x, and 4x
+        var_constraints.append(x1[row.start] == cp.sum([x[2] for x in open_intervals if x[1] == 1]))
+        var_constraints.append(x2[row.start] == cp.sum([x[2] for x in open_intervals if x[1] == 2]))
+        var_constraints.append(x4[row.start] == cp.sum([x[2] for x in open_intervals if x[1] == 4]))
 
-    hq.heappush(open_intervals, (end, cf, indicator[rowIdx]))
-    # output constraints at current time
-    onesum = cp.sum([x[2] for x in open_intervals if x[1] == 1])
-    twosum = cp.sum([x[2] for x in open_intervals if x[1] == 2])
-    foursum = cp.sum([x[2] for x in open_intervals if x[1] == 4])
 
-    step_constraints.append([onesum + q2 + q4 <= SET_SIZE])
+    indicator_constraint = [0 <= indicator, indicator <= 1]
+    objective = cp.Maximize(cp.sum(indicator))
 
-indicator_constraint = [0 <= indicator, indicator <= 1]
-objective = cp.Maximize(cp.sum(indicator))
+    constraints = it.chain(indicator_constraint,
+        # rest of variable constraints
+        var_constraints,
+        # equation s
+        [x1 + q2 + q4 <= set_size],
+        # equations 4c and 2c
+        [x4 <= 4 * q4, 4 * q4 <= x4 + 3],
+        [x2 <= 2 * q2, 2 * q2 <= x2 + 1],
+    )
 
-constraints = it.chain(indicator_constraint, step_constraints)
+    prob = cp.Problem(objective, constraints)
+    prob.solve();
+    print(prob)
+    print([x.value for x in indicator])
+    print(prob.status)
+    epsilon = 0.3
+    nearest = round(prob.value)
+    # for now, it appears to be floating point issue, and can't fix
+    # going to round around it and hope that works
+    # see: https://stackoverflow.com/questions/43194615/force-a-variable-to-be-an-integer-cvxpy
+    # see: https://stackoverflow.com/questions/55423345/cvxpy-integer-programming-returning-non-integer-solution
+    if abs(prob.value - nearest) > epsilon:
+        raise RuntimeError("Floating point imprecision too large to ignore")
+    return int(nearest)
 
-prob = cp.Problem(objective, constraints)
 
+if __name__ == '__main__':
+    parser = ArgumentParser(description="Creates an ILP from a CSV")
+    parser.add_argument('csv', type=FileType('r'), help='The source intervals to create the ILP from. Must be a readable file.')
+
+    args = parser.parse_args()
+
+    intervalreader = csv.reader(args.csv)
+    headers = next(intervalreader, None)
+    assert(headers[0] == 'start' and headers[1] == 'end' and headers[2] == 'cf' and len(headers) == 3)
+
+    intervals = [[int(y) for y in x] for x in intervalreader]
+    intervals = transformToNormalizedIntervals(intervals)
+
+    value = optimalForNormalizedIntervals(intervals)
+    #print("status:", prob.status)
+    print("optimal value", value)
---
+++ scripts/interval2ilp_test.py
@@ -0,0 +1,59 @@
+# run with python3 -m pytest interval2ilp_test.py
+
+from interval2ilp import transformToNormalizedIntervals, optimalForNormalizedIntervals
+
+def test_identity_transform():
+    test_identity = [
+        (0, 5, 4)
+    ]
+    resultIntervals, resultQuanta = transformToNormalizedIntervals(test_identity)
+    assert(len(resultIntervals) == 1)
+    assert((0, 1, 4) == resultIntervals[0].to_tuple())
+    assert(1 == resultQuanta)
+    assert(1 == optimalForNormalizedIntervals(resultIntervals, set_size=2))
+    assert(1 == optimalForNormalizedIntervals(resultIntervals, set_size=1))
+    assert(0 == optimalForNormalizedIntervals(resultIntervals, set_size=0))
+
+
+def test_nearly_intact_transform():
+    test_intervals = [
+        (0, 2, 4),
+        (3, 5, 2),
+        (4, 12, 1), # the only one that should be changed
+        (6, 8, 2),
+        (7, 10, 4),
+        (1, 9, 4)
+    ]
+    retIntervals, end_quanta = transformToNormalizedIntervals(test_intervals)
+    assert(end_quanta == 11)
+    assert(len(test_intervals) == len(retIntervals))
+    for interval in retIntervals:
+        if interval.start == 4:
+            assert(interval.end == 11 and interval.cf == 1)
+        else:
+            # it should be unchanged
+            tv = interval.to_tuple()
+            assert(tv in test_intervals)
+    #assert(4 == optimalForNormalizedIntervals(retIntervals, set_size=1))
+
+def test_enhanced_reordering():
+    test_intervals = [
+        (3, 12, 4),
+        (5, 39, 2),
+        (18, 22, 1),
+        (4, 32, 2)
+    ]
+    solutions = [
+        (0, 3, 4),
+        (2, 7, 2),
+        (4, 5, 1),
+        (1, 6, 2)
+    ]
+    retIntervals, end_quanta = transformToNormalizedIntervals(test_intervals)
+    # true as a general rule
+    print(retIntervals)
+    assert(end_quanta == len(test_intervals) * 2 - 1)
+    assert(len(test_intervals) == len(retIntervals))
+    for interval, solution in zip(retIntervals, solutions):
+        assert(interval.to_tuple() == solution)
+    assert(3 == optimalForNormalizedIntervals(retIntervals, set_size=1))
